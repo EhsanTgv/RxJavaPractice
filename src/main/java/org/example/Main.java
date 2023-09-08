5ff5b2749e21ce250cf0e4bccc1e34a86b51c80d
EhsanTgv <EhsanTgv@gmail.com>
2023-09-08 11:01:52 +0300

    Add Observable and observer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import io.reactivex.rxjava3.core.Observable;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Observable<String> observable = Observable.create(emitter -> {
+            emitter.onNext("Click no 1");
+            emitter.onNext("Click no 2");
+            emitter.onNext("Click no 3");
+            emitter.onComplete();
+        });
+
+        observable.subscribe(item -> {
+            System.out.println(item);
+        },throwable -> {
+            System.out.println(throwable.getMessage());
+        },()->{
+            System.out.println("On complete !");
+        });
     }
 }
