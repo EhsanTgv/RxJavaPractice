3a615f0fac4a7051fa51bd2d46d41311de88c3d2
EhsanTgv <EhsanTgv@gmail.com>
2023-10-17 10:48:09 +0300

    Implement Observable factory methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,68 @@
 package org.example;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Observable<String> observable = Observable.create(emitter -> {
-            emitter.onNext("Click no 1");
-            emitter.onNext("Click no 2");
-            emitter.onNext("Click no 3");
-            emitter.onComplete();
-        });
+        Observable<String> observable = Observable.just("Item 1", "Item 2");
 
-        observable.subscribe(item -> {
-            System.out.println(item);
-        },throwable -> {
-            System.out.println(throwable.getMessage());
-        },()->{
-            System.out.println("On complete !");
-        });
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+
+        Observable<Integer> numberObservable = Observable.fromIterable(numbers);
+
+        Observer<Integer> numberObserver = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                System.out.println(integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("On complete called!");
+            }
+        };
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("On complete called!");
+            }
+        };
+
+//        observable.subscribe(observer);
+        numberObservable.subscribe(numberObserver);
     }
 }
